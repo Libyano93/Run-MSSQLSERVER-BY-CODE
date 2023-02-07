d5d45d6ad0b863d8397dedca49c70ab12829e238
libyano93ly <younus93ly@gmail.com>
2023-02-07 16:15:21 +0200

    Initial Commit

---
+++ StopStartMSSqlServer/StopStartMSSqlServer/FrmMains.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace StopStartMSSqlServer
+{
+    partial class FrmMains
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.grgr = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(115, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 59);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.YellowGreen;
+            this.label1.Location = new System.Drawing.Point(115, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Testing Server";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(390, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1, 215);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(390, 1);
+            this.panel3.TabIndex = 5;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.White;
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel4.Location = new System.Drawing.Point(0, 1);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(1, 214);
+            this.panel4.TabIndex = 6;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.White;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel5.Location = new System.Drawing.Point(1, 214);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(389, 1);
+            this.panel5.TabIndex = 7;
+            // 
+            // grgr
+            // 
+            this.grgr.Dock = System.Windows.Forms.DockStyle.Top;
+            this.grgr.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.grgr.ForeColor = System.Drawing.Color.YellowGreen;
+            this.grgr.Location = new System.Drawing.Point(1, 1);
+            this.grgr.Name = "grgr";
+            this.grgr.Size = new System.Drawing.Size(389, 37);
+            this.grgr.TabIndex = 0;
+            this.grgr.Text = "RunStopMSSqlServer";
+            this.grgr.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(1, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(389, 1);
+            this.panel1.TabIndex = 8;
+            // 
+            // FrmMains
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(26)))), ((int)(((byte)(26)))));
+            this.ClientSize = new System.Drawing.Size(391, 215);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.grgr);
+            this.Controls.Add(this.panel5);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "FrmMains";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label grgr;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ StopStartMSSqlServer/StopStartMSSqlServer/FrmMains.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.ServiceProcess;
+using System.Windows.Forms;
+using System.Drawing;
+using Microsoft.VisualBasic;
+
+namespace StopStartMSSqlServer
+{
+    public partial class FrmMains : Form
+    {
+        public FrmMains()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                ServiceController service = new ServiceController("MSSQLSERVER", "DESKTOP-3EIH3C3");
+                if ((service.Status.Equals(ServiceControllerStatus.Stopped))
+                    | ((service.Status.Equals(ServiceControllerStatus.StopPending))))
+                {
+                    label1.Text = "السيرفر متوقف حاليا";
+                    label1.ForeColor = Color.Red;
+                    button1.Enabled = true;
+
+                }
+                else
+                {
+                    label1.Text = "السيرفر يعمل حاليا";
+                    label1.ForeColor = Color.Green;
+                    button1.Enabled = false;
+                }
+                
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        
+        }
+
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ServiceController service = new ServiceController("MSSQLSERVER", "DESKTOP-3EIH3C3");
+#pragma warning disable CA1416 // Validate platform compatibility
+                if ((service.Status.Equals(ServiceControllerStatus.Stopped))
+#pragma warning restore CA1416 // Validate platform compatibility
+                    | ((service.Status.Equals(ServiceControllerStatus.StopPending))))
+                {
+                    service.Start();
+                   
+                    label1.Text = "السيرفر يعمل حاليا";
+                    label1.ForeColor = Color.Green;
+                    Interaction.MsgBox("تم تفعيل السيرفر");
+                    button1.Enabled = false;
+                }
+
+            }
+            catch (Exception )
+            {
+                if (Interaction.MsgBox("لم نتمكن من تفعيل السيرف ", MsgBoxStyle.Exclamation, "الاتصال بالسيرفر") == MsgBoxResult.Retry)
+                {
+                    button1_Click(sender, e);
+                }
+            }
+        }
+
+
+    }
+
+}
